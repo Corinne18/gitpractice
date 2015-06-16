4e18c682d56e7d6ea98170858509ccc53b421a07
Corinne18 <cjrbnyc@gmail.com>
2015-06-16 14:07:22 +0000

    Added dog age

--- git_practice.rb
+++ git_practice.rb
@@ -1,4 +1,8 @@
-puts = "what is your name? "
+puts "what is your name? "
 name = gets
-
-puts "Hello " + name + " How are you?"
+puts "Hello " , name , " How are you?"
+puts "What is your age?"
+age = gets
+puts  "you are "  
+puts  age.to_i * 7 
+puts "years old in dog year"
