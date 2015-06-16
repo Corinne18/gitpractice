896b6a932e5a7a96744badebf393ebab6c672756
Corinne18 <cjrbnyc@gmail.com>
2015-06-16 14:20:47 +0000

    added new code

--- git_practice.rb
+++ git_practice.rb
@@ -3,6 +3,4 @@ name = gets
 puts "Hello " , name , " How are you?"
 puts "What is your age?"
 age = gets
-puts  "you are "  
-puts  age.to_i * 7 
-puts "years old in dog year"
+puts  "you are "  ,  age.to_i * 7 , "years old in dog year"
